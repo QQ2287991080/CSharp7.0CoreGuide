03c058513e2b6ee3c3bfc56b4484698d23191da8
Zero <2287991080@qq.com>
2020-04-11 15:36:09 +0800

    字符串格式化

--- CSharp7.0CoreGuide.App/Program.cs
+++ CSharp7.0CoreGuide.App/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Chapter4;
+using Chapter6;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,15 +31,15 @@ namespace CSharp7._0CoreGuide.App
             }
             {
                 //測試 字符串
-                Console.WriteLine("abcde abcde".IndexOf("CD", StringComparison.CurrentCultureIgnoreCase));
-                Console.WriteLine("abcd".IndexOfAny(new char[] { 'b' }));
-                string.Concat("a","b");
+                //Console.WriteLine("abcde abcde".IndexOf("CD", StringComparison.CurrentCultureIgnoreCase));
+                //Console.WriteLine("abcd".IndexOfAny(new char[] { 'b' }));
+                //string.Concat("a","b");
 
-                Console.WriteLine(string.Format("Name={0,-20}", "Zero")) ;
-                Console.WriteLine(string.Format("Name={0,-20}", "One")) ;
+                //Console.WriteLine(string.Format("Name={0,-20}", "Zero")) ;
+                //Console.WriteLine(string.Format("Name={0,-20}", "One")) ;
 
 
-                Console.WriteLine(string.Compare("A", "A"));
+                //Console.WriteLine(string.Compare("A", "A"));
                 //encoding支持的编码格式
                 //foreach (var item in Encoding.GetEncodings())
                 //{
@@ -45,10 +47,39 @@ namespace CSharp7._0CoreGuide.App
                 //}
 
                 //encoding将文本转为字节数组
-                byte[] utf8Byte = Encoding.UTF8.GetBytes("123456");
-                Console.WriteLine(utf8Byte.Length);
-                string utf8String = Encoding.UTF8.GetString(utf8Byte);
-                Console.WriteLine(utf8String);
+                //byte[] utf8Byte = Encoding.UTF8.GetBytes("123456");
+                //Console.WriteLine(utf8Byte.Length);
+                //string utf8String = Encoding.UTF8.GetString(utf8Byte);
+                //Console.WriteLine(utf8String);
+            }
+            {
+                //时间
+                //TimeSpan time = new TimeSpan(2,2,2);
+                //Console.WriteLine(time);
+                //var time2 =TimeSpan.FromDays(2);
+                //Console.WriteLine(time2);
+                //Console.WriteLine(DateTime.Now.TimeOfDay);
+                //Console.WriteLine(TimeSpan.Zero);
+                ////使用datetimeKind
+                //Console.WriteLine(new DateTime(5767,DateTimeKind.Local));
+                //var calender = new HebrewCalendar();
+                ////最大支持时间
+                //Console.WriteLine(calender.MaxSupportedDateTime);
+                ////最小支持时间
+                //Console.WriteLine(calender.MinSupportedDateTime);
+                //获取月份
+                //Console.WriteLine(calender.GetMonth(DateTime.Now));
+                //DateTime time3 = new DateTime(5767, 1, 1, new System.Globalization.HebrewCalendar());
+                //Console.WriteLine(time3);
+                //var t1 = DateTime.Now;
+                //var t2 = t1.AddDays(2);
+                //TimeSpan ts = t2 - t1;
+                //Console.WriteLine(ts.Ticks);
+                //Console.WriteLine(t1.ToString("o"));//指定与文化无关的格式化字符串
+                //Console.WriteLine(DateTime.Parse(t1.ToString("o")));
+
+                FormattingAndParsing.Invoker();
+
             }
             Console.ReadKey();
         }
---
+++ Chapter6/FormattingAndParsing.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6
+{
+    public class FormattingAndParsing
+    {
+        public static void Invoker()
+        {
+            /*格式化是将一个对象转化为字符串，解析则是将字符串转为对象。
+             在.netFramework中提供了一系列机制来处理这些操作。
+             1.ToString和Parse：这两个方法是很多类型默认具有的功能
+             2.格式提供器：对象上其他的ToString（以及Parse）重载方法会接受格式字符串或格式提供器。格式提供器不仅灵活而且与文化相关.Net Framework自带了数字类型和Datetime /Dattimeoffset的格式提供器
+             3.XmlConvert：这个静态类提供了基于Xml标准的格式化和解析方法。如果使用文化无关的转换，或希望避免误解析时，可以使用XmlConvert作为有效的通过转换器。XmlConvert支持数字类型、bool、datetime、Dattimeoffset、TimeSpan、Guid。
+             4.类转换器（Type converter）：这种转换器时面向设计器XAML解析器的。
+             */
+            {
+                //ToString和Parse
+                //ToString会为所有简单值类型产生有意义的字符输出
+                //这些值类型都定义了静态的Parse方法来完成反向的转换
+                string s = "1";
+                Console.WriteLine(s);
+                Console.WriteLine("解析为int类型");
+                int a = int.Parse(s);
+                Console.WriteLine(a);
+                //如果解析S失败那么系统会抛出异常（FormatException）,如果使用TryParse那么系统不会抛出异常，而时返回false
+                bool b = bool.TryParse(s, out bool isOk);
+                Console.WriteLine("解析为bool类型" + isOk);
+
+                //double germany= double.Parse("1.234", CultureInfo.InvariantCulture);
+                // Console.WriteLine(germany);
+            }
+            {
+                //格式化提供器：使用格式提供器的方式是IFormatProvider接口，所有的数字类型和Datetime类型都实现了这个接口(所有的enum都可以格式化但是它们没有特殊的IFormatter类)
+                //.Net Framework提供了三种格式提供器NumberFormatInfo,DateTimeFormatInfo,CultureInfo
+
+                //在格式提供器的上下文中，CultureInfo扮演了根据文化的区域设置返回NumberFormatInfo和DateTimeFormatInfo两个格式提供器的间接机制。
+                //使用适用于en-GB文化默认的NumberFormatInfo对象
+                Console.WriteLine("使用特殊文化-英国英语");
+                CultureInfo uk = CultureInfo.GetCultureInfo("en-GB");
+                Console.WriteLine(3.ToString("C", uk));
+                //使用不变的文化对DateTime进行格式化
+                DateTime dt = new DateTime(2020, 1, 1);
+                CultureInfo iv = CultureInfo.InvariantCulture;
+                Console.WriteLine(dt.ToString(iv));
+                Console.WriteLine(dt.ToString("d", iv));
+
+
+                //使用NumberFormatInfo和DateTimeFormatInfo
+                //实例化一个NumberFormatInfo对象并且将组分割从逗号修改为空格，并且它将数字格式化为保留小数点后三位的形式。
+                NumberFormatInfo f = new NumberFormatInfo();
+                f.NumberGroupSeparator = " ";
+                Console.WriteLine(12345.6789.ToString("N3", f));
+
+
+                //组合格式化
+                {
+                    var s = string.Format(CultureInfo.InvariantCulture, "{0}", "测试");
+                    Console.WriteLine(s);
+                }
+
+                {
+                    object obj = "测试";
+                    string s;
+                    if (obj is IFormattable)
+                    {
+                        s = ((IFormattable)obj).ToString(null, CultureInfo.InvariantCulture);
+                    }
+                    else if (obj == null)
+                    {
+                        s = "";
+                    }
+                    else
+                    {
+                        s = obj.ToString();
+                    }
+                    Console.WriteLine(s);
+                }
+
+                {
+                    //通过格式提供器进行解析
+                    /*格式提供器并未对解析提供标准结构。相反，每一个参与的类型都会重载它的静态Parse和TryParse方法接受一个格式提供器，以及一个可选的NumberStyles或DateTimeStyles枚举参数
+                     NumberStyles和DateTimeStyles的工作方式 ：它们提供了一些自定义的设置。
+                     */
+                    //例如允许括号或者货币符号出现在输入字符串中（默认这两个选项都是false）
+                    int minusTwo = int.Parse("(2)", NumberStyles.Integer | NumberStyles.AllowParentheses);
+                    Console.WriteLine(minusTwo);
+                    //decimal point = decimal.Parse("  5.20", NumberStyles.Currency, CultureInfo.GetCultureInfo("en-GB"));
+                    decimal point = decimal.Parse("  5.20", NumberStyles.Currency);
+                    Console.WriteLine(point);
+                }
+                {
+                    //IFormatProvider和ICustomFormatter
+                    /*所有的格式提供器都是实现了IFormatProvider。这个方法提供了一种间接进行格式化的手段CultureInfo就是用它来返回何时的NumberStyles和DateTimeStyles对象并完成格式化操作的
+                     实现IFormatProvider和ICustomFormatter，就能编写自定义的格式提供器（该自定义格式提供器可以和现有的配合工作）
+                     */
+
+                    IFormatProvider fp = new WordFormatProvider();
+                    Console.WriteLine(string.Format(fp, "{0:C} in words is{0:W}", -123.45));
+                }
+            }
+        }
+
+        
+    }
+    public class WordFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        static readonly string[] _numberWords= new string[] { "zero","one","two","three","four","five","six","seven", "eight", "nine","ten","minus","point"};
+        IFormatProvider _parent;
+        public WordFormatProvider() :this(CultureInfo.CurrentCulture) { }
+
+        public WordFormatProvider(IFormatProvider parent)
+        {
+            _parent = parent;
+        }
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null || format != "W") return string.Format(_parent, "{0:" + format + "}", arg);
+            StringBuilder builder = new StringBuilder();
+            string digitList = string.Format(CultureInfo.InvariantCulture, "{0}", arg);
+            foreach (char digit in digitList)
+            {
+                int i = "0123456789-.".IndexOf(digit);
+                if (i == -1) continue;
+                if (builder.Length > 0) builder.Append(' ');
+                builder.Append(_numberWords[i]);
+            }
+            return builder.ToString();
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter)) return this;
+            return null;
+        }
+    }
+}
