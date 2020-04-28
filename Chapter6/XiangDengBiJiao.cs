d58df7a62f6ae1d12598132c2064824b8f66795d
张力 <2287991080@qq.com>
2020-04-28 22:37:08 +0800

    ..

--- Chapter6/XiangDengBiJiao.cs
+++ Chapter6/XiangDengBiJiao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,10 +56,100 @@ namespace Chapter6
             Console.WriteLine("object.Equals静态方法");
             //object.Equals静态方法。
             //object 的类提供一个静态的辅助方法，该方法正是实现上一个例子中的AreEqual操作。虽然它们的名字与虚方法相同，但是它们不会冲突。
-            { 
-            
-            }
+            //这个方法可以提供null值的相等比较
+            {
+                object x = 1, y = 2;
+                object.Equals(x, y);
+                x = null;
+                object.Equals(x, y);
+                y = null;
+                object.Equals(x, y);
 
+                //泛型的使用
+            }
+            {
+                //重写IEquatable
+                var time1 = DateTime.Now;
+                Area area = new Area(5,3);
+                Area area2 = new Area(3, 5);
+                Console.WriteLine(area.Equals(area2));
+                Console.WriteLine(area == area2);
+                var time2 = DateTime.Now;
+               
+                Debug.WriteLine("结构体："+(time2 - time1).TotalSeconds);
+            }
+            {
+                var time1 = DateTime.Now;
+                Area2 area = new Area2(5, 3);
+                Area2 area2 = new Area2(3, 5);
+                Console.WriteLine(area.Equals(area2));
+                Console.WriteLine(area == area2);
+                var time2 = DateTime.Now;
+                Debug.WriteLine("类："+(time2 - time1).TotalSeconds);
+            }
         }
+
+        public class Test<T>
+        {
+            T _value;
+            public void SetValue(T value)
+            {
+                if (!object.Equals(value,_value))
+                {
+                    _value = value;
+                }
+            }
+        }
+    }
+    public class Area2
+    {
+        public int Measure1 { get; set; }
+        public  int Measure2 { get; set; }
+        public Area2(int m1, int m2)
+        {
+            Measure1 = Math.Min(m1, m2);
+            Measure2 = Math.Max(m1, m2);
+        }
+    }
+        public struct Area : IEquatable<Area>
+    {
+        public readonly int Measure1;
+        public readonly int Measure2;
+        public Area(int m1,int m2)
+        {
+            Measure1 = Math.Min(m1, m2);
+            Measure2 = Math.Max(m1, m2);
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Area)) return false;
+            return Equals((Area)obj);
+        }
+        /// <summary>
+        /// 由Json Bloch推荐的模式
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// int hash=17;
+        /// hash=hash*31+field1.GetHashCode();
+        /// hash=hash*31+field2.GetHashCode();
+        /// return hash;
+        /// </remarks>
+        /// <returns></returns>
+        public override int GetHashCode() => Measure1 + Measure2 * 31;
+        public override string ToString()
+        {
+            return (Measure1 + Measure2).ToString();
+        }
+        public bool Equals(Area other) => Measure1 == other.Measure1 && Measure2 == other.Measure2;
+
+        /// <summary>
+        /// 重载运算符
+        /// </summary>
+        /// <param name="a1"></param>
+        /// <param name="a2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Area a1, Area a2) => a1.Equals(a2);
+        public static bool operator !=(Area a1, Area a2) => !a1.Equals(a2);
     }
 }
